a7357419c6546c0035fae80cf35baa568cbda8ee
Walrus <81767572+jeffMullen@users.noreply.github.com>
2022-07-21 16:47:27 -0400

    creating basic UI card that will be used in both UserForm and user displays

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 import React from 'react';
 
+//Components
+import UserForm from './components/UserForm/UserForm';
+
 
 function App() {
   return (
     <div>
-
+      <UserForm></UserForm>
     </div>
   );
 }
---
+++ src/components/UserForm/UserForm.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+import Card from "../UI/Card";
+
+const UserForm = () => {
+  return (
+    <Card>
+      <form>
+        <label>Username</label>
+      </form>
+    </Card>
+  );
+};
+
+export default UserForm;
