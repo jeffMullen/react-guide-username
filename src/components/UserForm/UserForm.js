ecb0432392d5cb578793b273e7b7bd0ece0ff08f
Walrus <81767572+jeffMullen@users.noreply.github.com>
2022-07-21 17:03:18 -0400

    adding form elements with spacing

--- src/components/UserForm/UserForm.js
+++ src/components/UserForm/UserForm.js
@@ -2,11 +2,22 @@ import React from "react";
 
 import Card from "../UI/Card";
 
+import styles from "./UserForm.module.css";
+
 const UserForm = () => {
   return (
     <Card>
       <form>
-        <label>Username</label>
+        <div className={styles["form-controls"]}>
+          <div className={styles["sub-form"]}>
+            <label>Username</label>
+            <input type="text"></input>
+          </div>
+          <div className={styles["sub-form"]}>
+            <label>Age (Years)</label>
+            <input type="text"></input>
+          </div>
+        </div>
       </form>
     </Card>
   );
